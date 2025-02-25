60c6574733b8a207b93a9ca64d9fde60646952ab
DevOBR <bonilla_rdz@hotmail.com>
2025-02-25 15:39:52 -0600

    Adding Get by Id, Delete by Id and update app.http

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/games', (req, res) => {
-  res.json(games)
+  res.json({ games, totalCount: games.length })
+})
+
+app.get('/games/:id', (req, res) => {
+  const { id } = req.params
+  const game = games.find((game) => game.id === id)
+  if (!game) res.status(404).json({ message: 'Game not found' })
+
+  res.json(game)
+})
+
+app.delete('/games/:id', (req, res) => {
+  const { id } = req.params
+  const gameIndex = games.findIndex((game) => game.id === id)
+  if (gameIndex === -1) res.status(404).json({ message: 'Game not found' })
+
+  games.splice(gameIndex, 1)
+  res.json({ games, totalCount: games.length })
 })
 
 app.listen(PORT, () => {
