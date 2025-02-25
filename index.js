b20bbaf75fb08fdc9f82d09c521cbc2bcd3bea23
DevOBR <bonilla_rdz@hotmail.com>
2025-02-25 16:36:21 -0600

    Adding put and fixing post

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const crypto = require('node:crypto')
 const express = require('express')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 // const z = require('zod')
 const games = require('./games/games.json')
 
 const PORT = process.env.PORT ?? 53400
 
+app.disable('x-powered-by')
+
 // Middleware
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.get('/games', (req, res) => {
-  res.json({ games, totalCount: games.length })
+  return res.json({ games, totalCount: games.length })
 })
 
 app.get('/games/:id', (req, res) => {
   const { id } = req.params
-  const game = games.find((game) => game.id === id)
-  if (!game) res.status(404).json({ message: 'Game not found' })
+  const gameIndex = games.findIndex((game) => game.id === id)
+  if (gameIndex === -1) {
+    return res.status(404).json({ message: 'Game not found' })
+  }
 
-  res.json(game)
+  return res.json(games[gameIndex])
 })
 
 app.post('/games', (req, res) => {
-  if (!req.body) res.status(400).json({ message: 'Body is required' })
   const { name, img, year, rate } = req.body
+  const exist = games.some((game) => {
+    return game.name.toLowerCase() === name.toLowerCase()
+  })
+
+  if (exist) return res.status(500).json({ message: 'Game already exists' })
+  if (!req.body) return res.status(400).json({ message: 'Body is required' })
   const newGame = {
     name,
     img,
@@ -35,19 +44,37 @@ app.post('/games', (req, res) => {
 
   games.push({
     id: crypto.randomUUID(),
-    newGame
+    ...newGame
   })
 
-  res.status(201).json(newGame)
+  return res.status(201).json(newGame)
+})
+
+app.put('/games/:id', (req, res) => {
+  const { id } = req.params
+  const { name, img, year, rate } = req.body
+  const gameIndex = games.findIndex((game) => game.id === id)
+  if (gameIndex === -1) {
+    return res.status(404).json({ message: 'Game not found' })
+  }
+
+  games[gameIndex] = {
+    ...games[gameIndex],
+    ...{ name, img, year, rate }
+  }
+
+  return res.json(games[gameIndex])
 })
 
 app.delete('/games/:id', (req, res) => {
   const { id } = req.params
   const gameIndex = games.findIndex((game) => game.id === id)
-  if (gameIndex === -1) res.status(404).json({ message: 'Game not found' })
+  if (gameIndex === -1) {
+    return res.status(404).json({ message: 'Game not found' })
+  }
 
   games.splice(gameIndex, 1)
-  res.json({ games, totalCount: games.length })
+  return res.json({ games, totalCount: games.length })
 })
 
 app.listen(PORT, () => {
